66a834b9e59bd1e6d75ad57048734a531dec713c
Jaime Luna <jaime.paredes@di.unanleon.edu.ni>
2026-03-17 15:55:23 -0600

    feat: agregar comentario sobre la clave de Supabase en supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Reemplaza con tus credenciales de Supabase
 export const SUPABASE_URL = 'https://emmpppsumpchcpfrxpmn.supabase.co';
+// la clave es hackeable gracias :3
 export const SUPABASE_ANON_KEY = 'sb_publishable_NqWP03lm-pmfBKSXrJUJDg_HwtChM7M';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
